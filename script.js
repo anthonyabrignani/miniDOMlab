cea1bcdaa58f291830cad3a8b6beb3a9f6cd2cc4
anthony abrignani <anthony abrignani>
2021-04-18 18:35:53 -0400

    part 3 done and more framework

--- script.js
+++ script.js
@@ -39,6 +39,66 @@
   }
 }
 
+theForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let inputs = document.querySelectorAll("option");
+    let text = inputs[0].value;
+    let newListItem = document.createElement("li");
+    newListItem.innerText = `${text}`;
+    let myList = document.getElementById("coinspot");
+    myList.append(newListItem);
+    myList.setAttribute();
+    });
+
+
+
+
+document
+.getElementById("on")
+.addEventListener("click", turnOn);
+
+function turnOn() {
+    let bulbOn = document.getElementById("lightbulb");
+    bulbOn.style.backgroundColor = "gold";
+  }
+
+document
+.getElementById("off")
+.addEventListener("click", turnOff);
+
+function turnOff() {
+    let bulbOff = document.getElementById("lightbulb");
+    bulbOff.style.backgroundColor = "darkslateblue";
+  }
+
+document
+.getElementById("toggle")
+.addEventListener("click", toggle);
+
+function toggle() {
+    let toggle = document.getElementById("lightbulb");
+    if (toggle.style.backgroundColor === "gold") {
+    toggle.style.backgroundColor = "darkslateblue";
+    } else if (toggle.style.backgroundColor === "darkslateblue") {
+    toggle.style.backgroundColor = "gold";
+    }
+    
+  }
+
+document
+.getElementById("end")
+.addEventListener("click", endIt);
+
+function endIt() {
+    let end1 = document.getElementById("on");
+    end1.disabled = true;
+    let end2 = document.getElementById("off");
+    end2.disabled = true;
+    let end3 = document.getElementById("toggle");
+    end3.disabled = true;
+    let bulbGone = document.getElementById("lightbulb");
+    bulbGone.remove("lightbulb");
+  }
 
 
 
