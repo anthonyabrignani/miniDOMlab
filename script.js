7b5bad4289ae382b78b81c7a44ce662ffff63eac
anthony abrignani <anthony abrignani>
2021-04-18 22:20:44 -0400

    more changes, still stuck on 2

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 "use strict";
 {
-    let total = 0;
+  let total = 0.00;
 
-  document
-  .getElementById("lime")
-  .addEventListener("mousedown", costFunction1);
+  document.getElementById("lime").addEventListener("mousedown", costFunction1);
 
   function costFunction1() {
-    let price = 2.0;
+    let price = 2.00;
     document.getElementById("cost").innerText = total += price;
   }
 
@@ -16,7 +14,7 @@
     .addEventListener("mousedown", costFunction2);
 
   function costFunction2() {
-    let price = 3.0;
+    let price = 3.00;
     document.getElementById("cost").innerText = total += price;
   }
 
@@ -25,80 +23,84 @@
     .addEventListener("mousedown", costFunction3);
 
   function costFunction3() {
-    let price = 4.0;
+    let price = 4.00;
     document.getElementById("cost").innerText = total += price;
   }
 
-  document
-  .getElementById("fruit").
-  addEventListener("mousedown", costFunction4);
+  document.getElementById("fruit").addEventListener("mousedown", costFunction4);
 
   function costFunction4() {
-    let price = 6.0;
+    let price = 6.00;
     document.getElementById("cost").innerText = total += price;
   }
 }
 
+let theForm = document.getElementById("theForm");
+let options = document.getElementById("coins");
+let penny = document.getElementById("penny");
+let nickel = document.getElementById("nickel");
+let dime = document.getElementById("dime");
+let quarter = document.getElementById("quarter");
+let text1 = options[0].value;
+let text2 = options[1].value;
+let text3 = options[2].value;
+let text4 = options[3].value;
+let data = new FormData(theForm);
+
 theForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let inputs = document.querySelectorAll("option");
-    let text = inputs[0].value;
-    let newListItem = document.createElement("li");
-    newListItem.innerText = `${text}`;
-    let myList = document.getElementById("coinspot");
+  e.preventDefault();
+  let newListItem = document.createElement("li");
+  if (options.type === penny.type) {
+    newListItem.innerText = `${text1}`;
     myList.append(newListItem);
-    myList.setAttribute();
-    });
+  } else if (options.type === nickel.type) {
+    newListItem.innerText = `${text2}`;
+    myList.append(newListItem); 
+  } else if (options.type === dime.type) {
+    newListItem.innerText = `${text3}`;
+    myList.append(newListItem);   
+  } else if (options.type === quarter.type) {
+    newListItem.innerText = `${text4}`;
+    myList.append(newListItem);  
+  }
+  let myList = document.getElementById("coinspot");
+  newListItem.classList.add("coins");
+});
 
-
-
-
-document
-.getElementById("on")
-.addEventListener("click", turnOn);
+document.getElementById("on").addEventListener("click", turnOn);
 
 function turnOn() {
-    let bulbOn = document.getElementById("lightbulb");
-    bulbOn.style.backgroundColor = "gold";
-  }
+  let bulbOn = document.getElementById("lightbulb");
+  bulbOn.style.backgroundColor = "gold";
+}
 
-document
-.getElementById("off")
-.addEventListener("click", turnOff);
+document.getElementById("off").addEventListener("click", turnOff);
 
 function turnOff() {
-    let bulbOff = document.getElementById("lightbulb");
-    bulbOff.style.backgroundColor = "darkslateblue";
-  }
+  let bulbOff = document.getElementById("lightbulb");
+  bulbOff.style.backgroundColor = "darkslateblue";
+}
 
-document
-.getElementById("toggle")
-.addEventListener("click", toggle);
+document.getElementById("toggle").addEventListener("click", toggle);
 
 function toggle() {
-    let toggle = document.getElementById("lightbulb");
-    if (toggle.style.backgroundColor === "gold") {
+  let toggle = document.getElementById("lightbulb");
+  if (toggle.style.backgroundColor === "gold") {
     toggle.style.backgroundColor = "darkslateblue";
-    } else if (toggle.style.backgroundColor === "darkslateblue") {
+  } else if (toggle.style.backgroundColor === "darkslateblue") {
     toggle.style.backgroundColor = "gold";
-    }
-    
   }
+}
 
-document
-.getElementById("end")
-.addEventListener("click", endIt);
+document.getElementById("end").addEventListener("click", endIt);
 
 function endIt() {
-    let end1 = document.getElementById("on");
-    end1.disabled = true;
-    let end2 = document.getElementById("off");
-    end2.disabled = true;
-    let end3 = document.getElementById("toggle");
-    end3.disabled = true;
-    let bulbGone = document.getElementById("lightbulb");
-    bulbGone.remove("lightbulb");
-  }
-
-
-
+  let end1 = document.getElementById("on");
+  end1.disabled = true;
+  let end2 = document.getElementById("off");
+  end2.disabled = true;
+  let end3 = document.getElementById("toggle");
+  end3.disabled = true;
+  let bulbGone = document.getElementById("lightbulb");
+  bulbGone.remove("lightbulb");
+}
